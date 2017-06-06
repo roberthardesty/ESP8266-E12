6802b2393a0ea49e84f364ea543a9e2df9fd178c
robert <robert.hardesty@javs.com>
2017-06-06 00:44:29 +0000

    removed submodules

---
+++ ir-output/testRawCodes.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "irslinger.h"
+
+int main(int argc, char *argv[])
+{
+	uint32_t outPin = 23;            // The Broadcom pin number the signal will be sent on
+	int frequency = 38000;          // The frequency of the IR signal in Hz
+	double dutyCycle = 0.5;         // The duty cycle of the IR signal. 0.5 means for every cycle,
+	                                // the LED will turn on for half the cycle time, and off the other half
+
+	int codes[] = {9002, 4434, 608, 516, 608, 517, 607, 520, 606, 1636, 612, 1634, 615, 1634, 611, 516, 606, 519, 606, 1639, 611, 1636, 609, 1638, 608, 517, 608, 519, 605, 520, 607, 1640, 606, 1638, 609, 517, 608, 1636, 614, 1636, 610, 1636, 610, 515, 610, 519, 608, 513, 610, 515, 610, 1635, 616, 515, 605, 521, 606, 518, 607, 1637, 610, 1637, 608, 1637, 610, 1637, 610, 39926, 9116, 2167, 611};
+
+	int result = irSlingRaw(
+		outPin,
+		frequency,
+		dutyCycle,
+		codes,
+		sizeof(codes) / sizeof(int));
+	
+	return result;
+}
